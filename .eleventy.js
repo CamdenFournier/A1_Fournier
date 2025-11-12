8fd384a36d14261e7db038d29258744b034593fc
CamdenFournier <cfourni7@confederationcollege.ca>
2025-11-12 16:52:25 -0500

    finished website

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
 export default function (eleventyConfig) {
-eleventyConfig.addPassthroughCopy("style.css");
+    eleventyConfig.addPassthroughCopy("style.css");
+    eleventyConfig.addWatchTarget("style.css");
+    eleventyConfig.addPassthroughCopy("img");
 }
